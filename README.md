cc55cf61a92a99da22de5924b648bd4ee929f004
juanfe <juan.felipe.rueda@gmail.com>
2022-01-05 21:02:39 -0500

    finishing readme

--- README.md
+++ README.md
@@ -1,2 +1,38 @@
-# NbaTest
+# NbaTest for mach8
+
+TestNba is a program desing to solve the Mach Eight Sample Project
+
+## Description
+
+The idea is to find and print a list of all pairs of players
+whose height in inches adds up to the integer input to the application
+
+The list of all players will be obtained from a provided endpoint
+
+No external libraries are required to run this project just python 3
+
+## Usage
+
+To run the main program just use this line
+
+```bash
+py main.py 144
+```
+
+replace the 144 with your onw desired sum of heights
+
+## Testing 
  
+To run the tests 
+
+```bash
+py tests.py 
+```
+In this scenario will be tested just the main algorithm class to look for edge case scenarios
+
+
+## Contributing
+Main class will not be tested since there is no business logic to test there
+ 
+## License
+[MIT](https://choosealicense.com/licenses/mit/)
--- main.py
+++ main.py
@@ -10,13 +10,13 @@ with urllib.request.urlopen("https://mach-eight.uc.r.appspot.com/") as url:
 l = list()
 for i in players:
     l.append(int(i.get("h_in")))
-#we need to sort the players list since the algoritm will sort it too
+#we need to sort the players list since the algorithm  will sort it too
 #in this part the may have an O complexity of O(n.logn)
 players = sorted(players, key = lambda i: i['h_in'])
-#the algoritm is going to find the index of the matching pairs
+#the algorithm  is going to find the index of the matching pairs
 solution = findPairs.FindPairs(l,len(l),int(sys.argv[1])).findPairs()
 if len(solution) > 0 :
     for i in solution:
-        print(players[i[0]].get("h_in")+" "+players[i[0]].get("first_name") + " " + players[i[0]].get("last_name")+"          "+players[i[1]].get("h_in")+" "+players[i[1]].get("first_name")+" "+players[i[1]].get("last_name"))
+        print(players[i[0]].get("first_name") + " " + players[i[0]].get("last_name")+"          "+players[i[1]].get("first_name")+" "+players[i[1]].get("last_name"))
 else :
     print("No matches found")
